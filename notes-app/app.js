5d881ae50b4ec570771481183c987fd86715cd67
Barend Koorzen <koorzenb@gmail.com>
2021-03-11 12:14:22 +0200

    Working with yargs(2)

--- notes-app/app.js
+++ notes-app/app.js
@@ -4,11 +4,28 @@ const chalk = require("chalk");
 
 yargs.version("1.0.1");
 
+// command line : 
+//   node app.js add --title="To buy" --body="Carrots Cabbage" 
+
 yargs.command({
     command: "add",
     describe: "add a new note",
-    handler: function add() {
-        console.log("this is handler working");
+    builder: {
+        title: {
+            describe: "Adding a note",
+            demandOption: true,
+            type: "string"
+        },
+        body : {
+            describe: "This is body",
+            demandOption: true,
+            type: "string"
+        }
+    },
+    handler: function add(argv) {
+        console.log("Title: " + argv.title);
+        console.log("Body: " + argv.body);
     }
 })
-console.log(yargs.argv);
+
+yargs.parse();
