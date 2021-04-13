46badbf2d13316589157738e0bbe9a1d68e45558
Barend Koorzen <koorzenb@gmail.com>
2021-04-13 22:04:02 +0200

    Dynamic pages with templating
    1. Uses hbs

--- web-server/src/app.js
+++ web-server/src/app.js
@@ -3,15 +3,29 @@ const express = require('express');
 const app = express();
 
 const publicDirPath = path.join(__dirname, "../public");
+app.set('view engine', 'hbs');
 
 app.use(express.static(publicDirPath));
  
+app.get('', (req,res) => {
+    res.render('index', {
+        title: 'Weather App',
+        name: "BarendK"
+    });    // uses hbs - set above in app.set - to convert template to html
+})
+
 app.get('/help', (req,res) => {
-    res.send(path.join(__dirname, `../public/help.html`));
+    res.render('help', {
+        title: 'Help',
+        helpText: "Help is here"
+    }); 
 })
 
 app.get('/about', (req,res) => {
-    res.send(path.join(__dirname, `../public/about.html`));
+    res.render('about', {
+        title: 'About',
+        name: "Koos Kombuis"
+    }); 
 })
 
 app.get('/weather', (req,res) => {
