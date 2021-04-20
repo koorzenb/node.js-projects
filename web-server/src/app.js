99aca303f7de51b88d0629ea629f4c9f7db85ded
Barend Koorzen <koorzenb@gmail.com>
2021-04-20 20:21:22 +0200

    Customizing the Views directory

--- web-server/src/app.js
+++ web-server/src/app.js
@@ -2,9 +2,15 @@ const path = require('path');
 const express = require('express');
 const app = express();
 
+// Define paths for Express config
 const publicDirPath = path.join(__dirname, "../public");
-app.set('view engine', 'hbs');
+const viewsPath = path.join(__dirname, "../templates");
 
+// Setup handlebars egeni and views location
+app.set('view engine', 'hbs');
+app.set('views', viewsPath);
+
+// Setup static directory to serve
 app.use(express.static(publicDirPath));
  
 app.get('', (req,res) => {
