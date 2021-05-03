33dc571c102bd510250f97b5aba43012f4e779f5
Barend Koorzen <koorzenb@gmail.com>
2021-05-03 05:11:58 +0200

    Advanced Styling 2

--- weather-app/utils/geocode.js
+++ weather-app/utils/geocode.js
@@ -1,7 +1,7 @@
 const request = require("request");
 
 const geocode = (address, callback) => {
-    const url = `https://api.mapbox.com/geocoding/v5/mapbox.places/${encodeURIComponent(address)}.json?access_token=pk.eyJ1Ijoia29vcnplbmIiLCJhIjoiY2ttZTkzbXJ4MHBjOTJ1bnYxbXZtbGM2MCJ9.rmK2Q7C8klPmJ0wzY8pe_A`;
+    const url = `https://api.mapbox.com/geocoding/v5/mapbox.places/${encodeURIComponent(address)}.json?access_token=pk.eyJ1Ijoia29vcnplbmIiLCJhIjoiY2ttZTkzbXJ4MHBjOTJ1bnYxbXZtbGM2MCJ9.rmK2Q7C8klPmJ0wzY8pe_A&limit=1`;
     request({url, json: true}, (error, response) => {
         if(error) {
             callback("Unable to connect");
