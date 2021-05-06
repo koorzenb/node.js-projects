679c4229486e0449c6a2dbbcec830abbda7a39b2
Barend Koorzen <koorzenb@gmail.com>
2021-05-06 04:24:32 +0200

    WIring up the user interface

--- web-server/public/js/app.js
+++ web-server/public/js/app.js
@@ -4,10 +4,10 @@ const fetchWeather = (location) => {
     fetch(`http://localhost:3000/weather?address=${location}`).then((response) => {
         response.json().then((data) => {
             if(data.error) {
-                console.log(data.error);
+                console.log(messageOne.textContent = data.error);
             } else {
-                console.log(data.location);
-                console.log(data.forecast);
+                console.log(messageOne.textContent = data.location);
+                console.log(messageTwo.textContent = data.forecast);
             }
         })
     })
@@ -15,9 +15,13 @@ const fetchWeather = (location) => {
 
 const weatherForm = document.querySelector('form');
 const search = document.querySelector('input');
+const messageOne = document.querySelector('#message-1');
+const messageTwo = document.querySelector('#message-2');
 
 weatherForm.addEventListener('submit', e => {
     e.preventDefault();
+    messageOne.textContent = "Loading..."
+    messageTwo.textContent = null;
     const location = search.value;
     fetchWeather(location);
 })
