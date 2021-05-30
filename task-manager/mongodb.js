9eb68a1b24b0cf3a59928d1d69a87abcc2b315c6
Barend Koorzen <koorzenb@gmail.com>
2021-05-30 21:42:08 +0200

    Querying documents

--- task-manager/mongodb.js
+++ task-manager/mongodb.js
@@ -2,54 +2,36 @@ const {MongoClient,ObjectID} = require("mongodb");
 
 const connectionURL = 'mongodb://127.0.0.1:27017';
 const databaseName = 'task-manager';
-const id = new ObjectID();
-console.log(id);
+
 MongoClient.connect(connectionURL, { useNewUrlParser: true}, (error,client) => {
     if (error) return console.log("Unable to connect to database");
 
     const db = client.db(databaseName);
 
-    db.collection('users').insertOne({
-        _id: id,
-        name: 'Rita',
-        age: 40
-    }, (error, result) => {
-        if(error) return console.log("Unable to insert user");
+    // db.collection("users").findOne({name: "Rita"}, (error,user) => {
+    //     if(error) return console.log("Did not find user");
+    //     console.log(user);
+    // })
 
-        console.log(result.ops);
+    // db.collection("users").findOne({_id: new ObjectID("60b3dd526a12fd4d30a4e9d1")}, (error,user) => {
+    //     if(error) return console.log("Did not find user");
+    //     console.log(user);
+    // })
+
+    // First read: http://mongodb.github.io/node-mongodb-native/3.6/api/Collection.html#find
+    // then: follow hyperlink to Cursor
+    db.collection('users').find({age: 43}).toArray((error, users)=>{
+        console.log(users);
     })
 
-    // db.collection('users').insertMany([
-    //     {
-    //         name: "Andries",
-    //         age: 40
-    //     },
-    //     {
-    //         name: 'Barend',
-    //         age: 43
-    //     }
-    // ], (error, result) => {
-    //         if(error) return console.log("Unable to insert user");
-    //         console.log(result.ops);
-    //     }
-    // )
-    
-    // db.collection('tasks').insertMany([
-    //     {
-    //         description: "Do first task",
-    //         completed: true
-    //     },
-    //     {
-    //         description: "Do second task",
-    //         completed: true
-    //     },
-    //     {
-    //         description: "Do third task",
-    //         completed: false
-    //     }
-    // ], (error, result) => {
-    //         if(error) return console.log("Unable to insert task");
-    //         console.log(result.ops);
-    //     }
-    // )
+    //find last task by id
+    db.collection('tasks').findOne({_id: new ObjectID("60b3de6a320acd571809d69d")}, (error, task) => {
+        console.log(task);
+    })
+
+    //find all tasks not completed
+    db.collection('tasks').find({completed: false}).toArray((error,tasks) => {
+        console.log(tasks);
+    })
+
 });
