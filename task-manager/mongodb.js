69eea8d9a8f5180b51013b09a06b76bd380ef70f
Barend Koorzen <koorzenb@gmail.com>
2021-05-30 21:14:23 +0200

    ObjectID

--- task-manager/mongodb.js
+++ task-manager/mongodb.js
@@ -1,22 +1,23 @@
-const mongodb = require("mongodb");
-const MongoClient = mongodb.MongoClient;
+const {MongoClient,ObjectID} = require("mongodb");
 
 const connectionURL = 'mongodb://127.0.0.1:27017';
 const databaseName = 'task-manager';
-
+const id = new ObjectID();
+console.log(id);
 MongoClient.connect(connectionURL, { useNewUrlParser: true}, (error,client) => {
     if (error) return console.log("Unable to connect to database");
 
     const db = client.db(databaseName);
 
-    // db.collection('users').insertOne({
-    //     name: 'Barend',
-    //     age: 43
-    // }, (error, result) => {
-    //     if(error) return console.log("Unable to insert user");
+    db.collection('users').insertOne({
+        _id: id,
+        name: 'Rita',
+        age: 40
+    }, (error, result) => {
+        if(error) return console.log("Unable to insert user");
 
-    //     console.log(result.ops);
-    // })
+        console.log(result.ops);
+    })
 
     // db.collection('users').insertMany([
     //     {
@@ -33,22 +34,22 @@ MongoClient.connect(connectionURL, { useNewUrlParser: true}, (error,client) => {
     //     }
     // )
     
-    db.collection('tasks').insertMany([
-        {
-            description: "Do first task",
-            completed: true
-        },
-        {
-            description: "Do second task",
-            completed: true
-        },
-        {
-            description: "Do third task",
-            completed: false
-        }
-    ], (error, result) => {
-            if(error) return console.log("Unable to insert task");
-            console.log(result.ops);
-        }
-    )
+    // db.collection('tasks').insertMany([
+    //     {
+    //         description: "Do first task",
+    //         completed: true
+    //     },
+    //     {
+    //         description: "Do second task",
+    //         completed: true
+    //     },
+    //     {
+    //         description: "Do third task",
+    //         completed: false
+    //     }
+    // ], (error, result) => {
+    //         if(error) return console.log("Unable to insert task");
+    //         console.log(result.ops);
+    //     }
+    // )
 });
