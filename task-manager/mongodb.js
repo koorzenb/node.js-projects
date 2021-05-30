d058fa34752ac09bcf60c81739508e1e72151e80
Barend Koorzen <koorzenb@gmail.com>
2021-05-30 20:53:53 +0200

    Inserting documents

--- task-manager/mongodb.js
+++ task-manager/mongodb.js
@@ -9,8 +9,46 @@ MongoClient.connect(connectionURL, { useNewUrlParser: true}, (error,client) => {
 
     const db = client.db(databaseName);
 
-    db.collection('users').insertOne({
-        name: 'Barend',
-        age: 43
-    })
+    // db.collection('users').insertOne({
+    //     name: 'Barend',
+    //     age: 43
+    // }, (error, result) => {
+    //     if(error) return console.log("Unable to insert user");
+
+    //     console.log(result.ops);
+    // })
+
+    // db.collection('users').insertMany([
+    //     {
+    //         name: "Andries",
+    //         age: 40
+    //     },
+    //     {
+    //         name: 'Barend',
+    //         age: 43
+    //     }
+    // ], (error, result) => {
+    //         if(error) return console.log("Unable to insert user");
+    //         console.log(result.ops);
+    //     }
+    // )
+    
+    db.collection('tasks').insertMany([
+        {
+            description: "Do first task",
+            completed: true
+        },
+        {
+            description: "Do second task",
+            completed: true
+        },
+        {
+            description: "Do third task",
+            completed: false
+        }
+    ], (error, result) => {
+            if(error) return console.log("Unable to insert task");
+            console.log(result.ops);
+        }
+    )
 });
