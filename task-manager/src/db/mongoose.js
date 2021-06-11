e1c8e554826d9ddae317dff3d2cd85179e376304
Barend Koorzen <koorzenb@gmail.com>
2021-06-11 04:58:04 +0200

    Data Validation and Sanitization

--- task-manager/src/db/mongoose.js
+++ task-manager/src/db/mongoose.js
@@ -1,38 +1,68 @@
 const mongoose = require("mongoose");
+const validator = require("validator");
 
 mongoose.connect('mongodb://127.0.0.1:27017/task-manager-api', {
     useNewUrlParser: true,
     useCreateIndex: true
 });
 
-// const User = mongoose.model("User", {
-//     name: {
-//         type: String
-//     },
-//     age: {
-//         type: Number
-//     }
-// });
+const User = mongoose.model("User", {
+    name: {
+        type: String,
+        required: true,
+        trim: true
+    },
+    email: {
+        type: String,
+        required: true,
+        trim: true,
+        lowercase: true,
+        validate(value) {
+            if (!validator.isEmail(value)) {
+                throw new Error("Not valid email");
+            }
+        }
+    },
+    password: {
+        type: String,
+        required: true,
+        trim: true,
+        minLength: 6,
+        validate(value) {
+            if (value.toLowerCase().includes("password")) throw new Error("Cannot use 'password'")
+        }
+    },
+    age: {
+        type: Number,
+        default: 0,
+        validate(value) {
+            if (value < 0) throw new Error("Age must be a natural number")
+        }
+    }
+});
  
-// const me = new User({
-//     name: "barend",
-//     age: 43
-// })
+const me = new User({
+    name: "Barend     ",
+    email: "     barend@GMSIL.com    ",
+    password: "Password"
+})
 
 // me.save().then(() => console.log(me)).catch(error => console.log(error))
 
 const Task = mongoose.model('Task', {
     description: {
-        type: String
+        type: String,
+        required: true,
+        trim: true
     },
     completed: {
-        type: Boolean
+        type: Boolean,
+        default: false
     }
 })
 
 const task = new Task({
-    description: "writing to Robo 3T",
-    completed: false
+    description: "testing required",
 })
 
 task.save()
