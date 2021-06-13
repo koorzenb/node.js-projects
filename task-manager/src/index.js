b2b955b822bdbaae5112ed9f180c15106fe35551
Barend Koorzen <koorzenb@gmail.com>
2021-06-13 22:05:31 +0200

    Create Resource Endpoints (tasks)

--- task-manager/src/index.js
+++ task-manager/src/index.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const Task = require("./models/task");
 require("./db/mongoose");
 const User = require("./models/user");
 
@@ -9,12 +10,18 @@ app.use(express.json());
 
 app.post('/users', (req,res) => {
     const user = new User(req.body);
-    user.save().then(()=> {
-        res.send(user)
-    }).catch(error => {
-        res.status(400).send(error);
-    })
 
+    user.save()
+    .then(()=> res.status(201).send(user))
+    .catch(error => res.status(400).send(error))
+})
+
+app.post('/tasks', (req,res) => {
+    const task = new Task(req.body);
+
+    task.save()
+    .then(()=> res.status(201).send(task))
+    .catch(error => res.status(400).send(error));
 })
 
 app.listen(port, () => console.log(`Server is up on port ${port}`));
