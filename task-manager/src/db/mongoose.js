6acc57bb7b25af8193b51b82dac22315383b5b52
Barend Koorzen <koorzenb@gmail.com>
2021-06-24 20:56:23 +0200

    Setup on homePC

--- task-manager/src/db/mongoose.js
+++ task-manager/src/db/mongoose.js
@@ -2,5 +2,9 @@ const mongoose = require("mongoose");
 
 mongoose.connect('mongodb://127.0.0.1:27017/task-manager-api', {
     useNewUrlParser: true,
-    useCreateIndex: true
+    useCreateIndex: true,
+    useUnifiedTopology: true,
+}).then(() => console.log('...db Connected!'))
+.catch(err => {
+    console.log(`DB Connection Error: ${err.message}`);
 });
