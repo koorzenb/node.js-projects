eb2328d3cfe9801ba8381d6730bb82cb6c4c0e77
Barend Koorzen <koorzenb@gmail.com>
2021-06-24 21:39:33 +0200

    Resource Updating Endpoints

--- task-manager/src/index.js
+++ task-manager/src/index.js
@@ -3,6 +3,8 @@ const Task = require("./models/task");
 require("./db/mongoose");
 const User = require("./models/user");
 
+// see https://mongoosejs.com/docs/queries.html for more methods
+
 const app = express();
 const port = process.env.PORT || 3000;
 
@@ -29,9 +31,30 @@ app.get('/users', async (req,res) => {
     }
 })
 
+app.patch('/users/:id', async (req,res) => {
+    const updates = Object.keys(req.body);
+    console.log(updates);
+    const allowedUpdates = ["name", "email", "password", "age"];
+    const isValidOperations = updates.every(update => allowedUpdates.includes(update));
+    console.log(isValidOperations);
+
+    if(!isValidOperations) {
+        return res.status(400).send( {error: "InVaLiD update"})
+    }
+
+    try {
+        const user = await User.findByIdAndUpdate(req.params.id, req.body, {new: true, runValidators: true});
+        if(!user) {
+            return res.status(404).send();
+        }
+        res.send(user);
+    } catch (error) {
+        res.status(400).send(error)
+    }
+})
+
 app.get('/users/:id', async (req,res) => {
     const _id = req.params.id;
-    console.log(_id);
 
     try {
         const user = await User.findById(_id);
@@ -66,16 +89,38 @@ app.get("/tasks", async (req,res) => {
 })
 
 app.get('/tasks/:id', async (req,res) => {
-    const _id = req.params.id;
 
     try {
-        const task = await Task.findById(_id);
+        const task = await Task.findById(req.params.id);
         if(!task) {
             return res.status(404).send();
         }
         res.send(task);
     } catch (error) {
         res.status(500).send(error);        
+    }
+})
+
+
+
+app.patch('/tasks/:id', async (req,res) => {
+    const updates = Object.keys(req.body);
+    const allowedUpdates = ["description", "completed"];
+    const isValidOperations = updates.every(update => allowedUpdates.includes(update));
+    console.log(isValidOperations);
+    
+    if(!isValidOperations) {
+        return res.status(400).send( {error: "InVaLiD update"})
+    }
+
+    try {
+        const task = await Task.findByIdAndUpdate(req.params.id, req.body, {new: true, runValidators: true});
+        if(!task) {
+            return res.status(404).send();
+        }
+        res.send(task);
+    } catch (error) {
+        res.status(400).send(error);        
     }
 })
 
