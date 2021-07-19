7fc3471d0b3ac1991944f1dfbc152a92cd2a9f15
Barend Koorzen <koorzenb@gmail.com>
2021-07-19 04:19:48 +0200

    Update user.js

--- task-manager/src/models/user.js
+++ task-manager/src/models/user.js
@@ -3,7 +3,7 @@ const validator = require("validator");
 const bcrypt = require('bcryptjs'); 
 const jwt = require("jsonwebtoken");
 
-const userSchema = mongoose.Schema({
+const userSchema = new mongoose.Schema({
     name: {
         type: String,
         required: true,
