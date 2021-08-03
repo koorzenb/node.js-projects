2351ff2d9b73421821c827473a60d3738974720a
Barend Koorzen <koorzenb@gmail.com>
2021-08-03 04:57:49 +0200

    Auto-Cropping and Image Formatting

--- task-manager/src/index.js
+++ task-manager/src/index.js
@@ -21,33 +21,6 @@ const port = process.env.PORT || 3000;
 //         res.status(503).send("Service down for maintenance");
 // })
 
-const multer = require('multer');
-const upload = multer({
-    dest: 'images',
-    limits: {
-        fileSize: 1000000
-    },
-    fileFilter(req, file, cb) {
-        // see https://www.npmjs.com/package/multer -> API for more info
-        // if(!file.originalname.endsWith('.pdf')) {
-            //     return cb(new Error('Please upload PDF'))
-            // }
-            
-            if(!file.originalname.match(/\.(doc|docx)$/)) {
-                return cb(new Error('Please upload a Word doc'))
-            }
-        // cb(new Error('File must be a pdf'));
-        cb(undefined, true);
-        // cb(undefined, false);
-    }
-})
-
-app.post('/upload' ,upload.single('upload'), (req,res) => {
-    res.send()
-}, (error, req, res, next) => {
-    res.status(400).send({error: error.message});
-})
-
 app.use(express.json());
 app.use(userRouter);
 app.use(taskRouter);
