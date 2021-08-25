8825c470c9b2d45112d4dfef9cdb36a1082d213f
Barend Koorzen <koorzenb@gmail.com>
2021-08-25 22:05:50 +0200

    Wrapping up User Tests

--- task-manager/tests/user.test.js
+++ task-manager/tests/user.test.js
@@ -97,3 +97,36 @@ test('Should not delete account for unauth user', async () => {
         .send()
         .expect(401);
 })
+
+test('Should upload avatar image', async () => {
+    await request(app)
+        .post('/users/me/avatar')
+        .set("Authorization", `Bearer ${userOne.tokens[0].token}`)
+        .attach('avatar', 'tests/fixtures/profiles-pic.jpg')
+        .expect(200)
+    const user = await User.findById(userOneId);
+    expect(user.avatar).toEqual(expect.any(Buffer))
+})
+
+test('Should update valid user fields', async () => {
+    await request(app)
+        .patch('/users/me')
+        .set("Authorization", `Bearer ${userOne.tokens[0].token}`)
+        .send({
+            name: 'Rita'
+        })
+        .expect(200)
+
+    const user = await User.findById(userOneId)
+    expect(user.name).toEqual('Rita')
+})
+
+test('Should not update invalid user fields', async () => {
+    await request(app)
+        .patch('/users/me')
+        .set("Authorization", `Bearer ${userOne.tokens[0].token}`)
+        .send({
+            location: 'Cape Town'
+        })
+        .expect(400)
+})
