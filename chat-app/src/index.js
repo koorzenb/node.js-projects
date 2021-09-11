ce07cd849eeb3c1f4618338b28cb41abbe762678
Barend Koorzen <koorzenb@gmail.com>
2021-09-11 05:42:29 +0200

    Getting Started with Socket.io

--- chat-app/src/index.js
+++ chat-app/src/index.js
@@ -1,18 +1,24 @@
 const path = require('path');
 const express = require('express');
-const hbs = require('hbs');
+const http = require('http');
+const socketio = require("socket.io");
 
 const app = express();
+const server = http.createServer(app);
+const io = socketio(server);
+
 const port = process.env.PORT || 3000;
 
 // Define paths for Express config
 const publicDirPath = path.join(__dirname, "../public");
-const viewsPath = path.join(__dirname, "../templates/views");
-const partialsPath = path.join(__dirname, "../templates/partials");
 
 // Setup static directory to serve
 app.use(express.static(publicDirPath));
 
-app.listen(port, () => {
+io.on('connection', () => {
+    console.log("New WebSocket connection");
+})
+
+server.listen(port, () => {
     console.log(`Server is up on port ${port}`);
 })
