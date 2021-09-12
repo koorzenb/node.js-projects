a145fa69be7ca222946bcefd465608454eeafec2
Barend Koorzen <koorzenb@gmail.com>
2021-09-12 05:52:28 +0200

    Broadcasting Events

--- chat-app/public/js/chat.js
+++ chat-app/public/js/chat.js
@@ -1,6 +1,6 @@
 const socket = io()
 
-socket.on('broadcast', (message) => {
+socket.on('message', (message) => {
     console.log(message)
 })
 
--- chat-app/src/index.js
+++ chat-app/src/index.js
@@ -15,10 +15,15 @@ app.use(express.static(publicDirectoryPath))
 io.on('connection', (socket) => {
     console.log('New WebSocket connection')
 
-    socket.emit('welcome', 'message sent')
+    socket.emit('message', 'Welcome!');
+    socket.broadcast.emit("message","A new user has joined");
 
     socket.on('sendMessage', (message) => {
         io.emit('broadcast', message)
+    })
+
+    socket.on('disconnect', () => {
+        io.emit("message", "A user has left");
     })
 })
 
