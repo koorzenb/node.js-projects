bfe781b67e0d4dc67e82a4f7f60ec7902b2d9315
Barend Koorzen <koorzenb@gmail.com>
2021-09-19 21:22:26 +0200

    Rendering Location Messages

--- chat-app/public/js/chat.js
+++ chat-app/public/js/chat.js
@@ -7,12 +7,21 @@ const $messages = document.querySelector("#messages");
 
 // Templates
 const messageTemplate = document.querySelector("#message-template").innerHTML;
+const locationTemplate = document.querySelector("#location-template").innerHTML;
 
 
 socket.on('message', (message) => {
     console.log(message);
     const html = Mustache.render(messageTemplate, {
         message
+    });
+    $messages.insertAdjacentHTML('beforeend', html);
+})
+
+socket.on('locationMessage', (location) => {
+    console.log(location);
+    const html = Mustache.render(locationTemplate, {
+        location
     });
     $messages.insertAdjacentHTML('beforeend', html);
 })
