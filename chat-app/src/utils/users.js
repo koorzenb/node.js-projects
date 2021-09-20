20b11e4257d3491e81279769b2929e97a19e1aac
Barend Koorzen <koorzenb@gmail.com>
2021-09-20 20:51:12 +0200

    Storing Users

---
+++ chat-app/src/utils/users.js
@@ -0,0 +1,34 @@
+const users = [];
+
+const addUser = ({ id, username, room}) => {
+    username = username.trim().toLowerCase();
+    room = room.trim().toLowerCase();
+    
+    if(!username || !room) {
+        return {
+            error: 'Username and room are required'
+        }
+    }
+
+    const existingUser = users.find( user => {
+        return user.room === room && user.username === username;
+    })
+
+    if(existingUser) {
+        return {
+            error: 'Username is in use'
+        }
+    }
+
+    const user = { id, username, room }
+    users.push(user);
+    return {user}
+}
+
+addUser({
+    id: 22,
+    username: '  Barend',
+    room: 'city   '
+})
+
+console.log(users);
